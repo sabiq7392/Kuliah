3615da6884417ad53504938a29b8ef28d00f1c4f
Sabiq Muhammad <sabi20153ti@student.nurulfikri.ac.id>
2021-10-25 02:54:06 +0700

    add 1

--- SM2-Praktikum-4-PHP-Codeigniter-MVC-main/application/models/Bmipasien_model.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Bmipasien_model extends CI_Model {
-    public $id;
-    public $tanggal;
-    public $pasien;
-    public $bmi;
-}
--- SM2-Tugas-Akhir/application/models/Menu_model.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Menu_model extends CI_Model {
-    public function whoCanAccessMenu() {
-        $query = $this->db->get('users_menu');
-        return $query->result();
-    }
-}
